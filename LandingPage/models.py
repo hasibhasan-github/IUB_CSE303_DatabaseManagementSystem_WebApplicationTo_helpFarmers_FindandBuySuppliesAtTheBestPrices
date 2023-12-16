d65f3802025fdb02710d63be362a02e44638c5f1
hasibhasan-github <2220166@iub.edu.bd>
2023-12-17 05:44:34 +0600

    Commited by @hasibhasan-github

--- LandingPage/models.py
+++ LandingPage/models.py
@@ -8,7 +8,6 @@ class User(AbstractUser):
     USER_TYPE_CHOICES = [
         ('supplier', 'Supplier'),
         ('farmer', 'Farmer'),
-        ('ARDOFFICER', 'ARDOFFICER'),
     ]
     email = models.EmailField('Email address', unique=True)
     user_type = models.CharField('User Type', max_length=20, choices=USER_TYPE_CHOICES, blank=True, null=True)
