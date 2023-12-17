5003861d1b81522b7d8cd545a55e5113e028acfa
Md Masum Billa <141360068+Masum0895@users.noreply.github.com>
2023-12-17 10:57:17 +0600

    git commit -m "Commited by @Masum0895"

--- ARDOfficer/views.py
+++ ARDOfficer/views.py
@@ -4,4 +4,19 @@ from django.shortcuts import render
 
 
 def view(request):
-    return render(request, 'ARDOfficer/a.html')
+    return render(request, 'ARDOfficer/Consultant_Dashboard.html')
+
+def base(request):
+     return render(request, ('ARDOfficer/base.html'))
+
+def Contact(request):
+    return render(request, ('ARDOfficer/contact.html'))
+
+def Research_news(request):
+    return render(request, ('ARDOfficer/research_news.html'))
+
+def Farmer_data(request):
+    return render(request, ('ARDOfficer/Farmer_data.html'))
+
+def Financial_plan(request):
+     return render(request, ('ARDOfficer/Financial_Plan.html'))
